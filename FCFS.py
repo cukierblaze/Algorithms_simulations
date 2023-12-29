3daebefb4aab3108383f6207ab3b157e725cb995
unknown <cukierblazej@gmail.com>
2023-12-29 19:08:10 +0100

    Another modifies to project

--- FCFS.py
+++ FCFS.py
@@ -4,25 +4,30 @@ def FCFS(processes):
     queue=[]
     occupied =False
     executed_processes=[]
+    burst = None
 
-    for time in range(1,sum(p.burst_time + p.arrival_time for p in processes) + 1):
+    for time in range(0, sum(p.burst_time + p.arrival_time for p in processes) + 1):
         for proc in processes:
-            if proc.arrival_time <= time and proc not in executed_processes:
+            if proc.arrival_time == time and proc not in executed_processes:
                 if not occupied:
                     executed_processes.append(proc)
                     occupied = True
+                    burst = proc.burst_time
                     print(f"Process {proc.name} executed at time {time}")
                 else:
                     queue.append(proc)
-        if queue:
-            next_proccess = queue.pop(0)
-            executed_processes.append(next_proccess)
-            occupied=True
-            print(f"Process {next_proccess.name} executed at time {time}")
-        occupied=False
+        if burst == 0:
+            if queue:
+                burst = queue[0].burst_time
+                print(f"Process {queue[0].name} executed at time {time}")
+                executed_processes.append(queue.pop(0))
+            else:
+                occupied = False
+        if burst:
+            burst -= 1
     return executed_processes
 
-
+""""
 if __name__ == "__main__":
     num_processes = int(input("Enter the number of processes: "))
     a = int(input("Enter the lowest border of arrival_time: "))
@@ -42,3 +47,5 @@ if __name__ == "__main__":
     processes_to_schedule = generator_instance.generator(num_processes,a,b,c,d)
 
     scheduled_processes = FCFS(processes_to_schedule)
+"""
+FCFS([Process('a', 1, 4), Process('b', 1, 3), Process('c', 3, 2)])
--- SJF.py
+++ SJF.py
@@ -1,29 +1,42 @@
 from processes import Process
 from generator import Generator
-def SJF(processes):
-    queue=[]
-    occupied =False
-    executed_processes=[]
+'''
+    proces1 = Process("a", 22, 15)
+    proces2 = Process("b", 7, 4)
+    proces3 = Process("c", 34, 8)
+    proces4 = Process("d", 6, 9)
+    proces5 = Process("e", 28, 15)
+    proces6 = Process("f", 90, 104)
+    '''
 
-    for time in range(1,sum(p.burst_time + p.arrival_time for p in processes) + 1):
+def SJF(processes):
+    queue = []
+    occupied = False
+    executed_processes = []
+    burst = None
+
+    for time in range(0, sum(p.burst_time + p.arrival_time for p in processes) + 1):
         for proc in processes:
-            if proc.arrival_time <= time and proc not in executed_processes:
-                if not occupied:
-                    executed_processes.append(proc)
-                    occupied = True
-                    print(f"Process {proc.name} executed at time {time}")
-                else:
-                    queue.append(proc)
-        if queue:
-            processes.sort(key=lambda x: x.burst_time)
-            next_proccess = queue.pop(0)
-            executed_processes.append(next_proccess)
-            occupied=True
-            print(f"Process {next_proccess.name} executed at time {time}")
-        occupied=False
+            if proc.arrival_time == time and proc not in executed_processes:
+                queue.append(proc)
+                queue.sort(key=lambda x: x.burst_time)
+        if not occupied and queue:
+            burst = queue[0].burst_time
+            print(f"Process {queue[0].name} executed at time {time}")
+            executed_processes.append(queue.pop(0))
+            occupied = True
+        if burst == 0:
+            if queue:
+                burst = queue[0].burst_time
+                print(f"Process {queue[0].name} executed at time {time}")
+                executed_processes.append(queue.pop(0))
+            else:
+                occupied = False
+        if burst:
+            burst -= 1
     return executed_processes
 
-
+'''
 if __name__ == "__main__":
     num_processes = int(input("Enter the number of processes: "))
     a = int(input("Enter the lowest border of arrival_time: "))
@@ -43,3 +56,6 @@ if __name__ == "__main__":
     processes_to_schedule = generator_instance.generator(num_processes,a,b,c,d)
 
     scheduled_processes = SJF(processes_to_schedule)
+'''
+SJF([Process('a', 1, 4), Process('b', 1, 3), Process('c', 4, 8), Process('d', 5, 1)])
+
